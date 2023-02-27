2da31089e098db499d4e09474ad1abdd7d8c6292
Kenneth High <kennethhigh7@gmail.com>
2023-02-27 10:43:10 +0000

    commit Array Contains Lab

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,11 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        for (int itor:arr){
+            if(itor == target){
+                return true;
+            }
+        }
         return false;
     }
 }
